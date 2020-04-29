68b574e94e3f5422c3cc6dc73ed224a67fa75b83
Bonnie Yip <byip@altriva.com>
2020-04-29 16:00:29 -0700

    Updates

---
+++ component---src-pages-project-section-js-aa74ca1b2eb830df2b30.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{FQZ3:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),r=t("7vrA");n.default=function(e){var n=e.title,t=e.description;return l.a.createElement(r.a,null,l.a.createElement("h1",null,n),l.a.createElement("p",null,t))}}}]);
+//# sourceMappingURL=component---src-pages-project-section-js-aa74ca1b2eb830df2b30.js.map
---
+++ styles-1840e6e2af97c6fd7416.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{"8ypT":function(n,o,p){},q4sD:function(n,o,p){},ydly:function(n,o,p){}}]);
+//# sourceMappingURL=styles-1840e6e2af97c6fd7416.js.map
